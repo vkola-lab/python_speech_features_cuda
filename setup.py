4c8795d0be30decff6f74731402611063c3a36bc
cxue2 <cxue2@bu.edu>
2020-08-07 05:12:28 -0400

    0.0.8

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import setuptools
 
 setuptools.setup(
     name='python_speech_features_cuda',
-    version='0.0.7',
+    version='0.0.8',
     author='Chonghua Xue',
     author_email='cxue2@bu.edu',
     url='https://github.com/vkola-lab/python_speech_features_cuda',
